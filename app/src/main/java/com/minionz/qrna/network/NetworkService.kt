3dddc6aeecff8f18f401d3f3d69044c8bc47b31c
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-03 15:11:08 +0900

    feat : 회원가입 요청 API Interface에 작성

--- app/src/main/java/com/minionz/qrna/network/NetworkService.kt
+++ app/src/main/java/com/minionz/qrna/network/NetworkService.kt
@@ -1,4 +1,15 @@
 package com.minionz.qrna.network
 
+import com.minionz.qrna.data.SIgnUpResponseData
+import com.minionz.qrna.data.SignUpRequestData
+import retrofit2.Call
+import retrofit2.http.POST
+
 interface NetworkService {
+
+    @POST("/api/v1/users/join")
+    fun signUp(
+        signUpRequestBody : SignUpRequestData
+    ) : Call<SIgnUpResponseData>
+
 }
--- app/src/main/java/com/minionz/qrna/network/RetrofitBuilder.kt
+++ app/src/main/java/com/minionz/qrna/network/RetrofitBuilder.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
-    private const val baseUrl = "http://ec2-15-164-13-85.ap-northeast-2.compute.amazonaws.com:8080"
+    private const val baseUrl = ""
 
     fun getRetrofit() : Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create())
