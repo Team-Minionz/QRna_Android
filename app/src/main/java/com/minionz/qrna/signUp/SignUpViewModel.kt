ad57ef65ee573c2ed8fe99edab2adcd7ee2efa6b
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-03 16:17:06 +0900

    feat : 회원가입을 위한 변수들을 ViewModel에 선언

--- app/src/main/java/com/minionz/qrna/signUp/SignUpViewModel.kt
+++ app/src/main/java/com/minionz/qrna/signUp/SignUpViewModel.kt
@@ -1,6 +1,16 @@
 package com.minionz.qrna.signUp
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SignUpViewModel : ViewModel() {
+
+    val signUpName = MutableLiveData<String>()
+    val signUpEmail = MutableLiveData<String>()
+    val signUpNickName = MutableLiveData<String>()
+    val signUpPhone = MutableLiveData<String>()
+    val signUpPassword = MutableLiveData<String>()
+
+    val userId = MutableLiveData<Int>().apply { value = 1 }
+
 }
