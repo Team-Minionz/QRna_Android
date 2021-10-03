9fecd32d9f9e47e3a1c5142fefa113dde6801317
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-03 16:15:46 +0900

    feat : Layout 파일에 DataBinding 적용

--- app/src/main/java/com/minionz/qrna/viewModel/MainViewModel.kt
+++ app/src/main/java/com/minionz/qrna/viewModel/MainViewModel.kt
@@ -1,6 +1,10 @@
 package com.minionz.qrna.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    val userId = MutableLiveData<String>().apply { this.value = "" }
+
 }
