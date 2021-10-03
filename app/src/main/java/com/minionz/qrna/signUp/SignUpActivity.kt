3b824d94a3c463bae9d33584874f177d2fd8912b
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-03 16:16:38 +0900

    feat : Activity 에서 DataBinding, ViewModel을 사용하기 위한 초기화 적용

--- app/src/main/java/com/minionz/qrna/signUp/SignUpActivity.kt
+++ app/src/main/java/com/minionz/qrna/signUp/SignUpActivity.kt
@@ -2,11 +2,26 @@ package com.minionz.qrna.signUp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.minionz.qrna.R
+import com.minionz.qrna.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignUpBinding
+    private lateinit var viewModel : SignUpViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        init()
+    }
+
+    private fun init() {
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+        viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
