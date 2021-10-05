19eb1332d5c63d672095be3038f770d29fa08e4c
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-05 14:21:34 +0900

    feat : Login 화면의 Data Binding 적용

--- app/src/main/java/com/minionz/qrna/LoginActivity.kt
+++ app/src/main/java/com/minionz/qrna/LoginActivity.kt
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import com.minionz.qrna.databinding.ActivityMainBinding
+import com.minionz.qrna.databinding.ActivityLoginBinding
 import com.minionz.qrna.viewModel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityLoginBinding
     private lateinit var viewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
