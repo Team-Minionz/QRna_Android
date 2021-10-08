2bc36cbdf246e1380d53ed8c6a45fef460f090a7
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-08 16:03:10 +0900

    feat : QR 인식 성공 시 QR 인증 API 요청하는 기능 추가

--- app/src/main/java/com/minionz/qrna/view/qr/QrCodeReaderActivity.kt
+++ app/src/main/java/com/minionz/qrna/view/qr/QrCodeReaderActivity.kt
@@ -7,6 +7,12 @@ import android.util.Log
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 import com.minionz.qrna.R
+import com.minionz.qrna.data.QrCertificationRequestData
+import com.minionz.qrna.data.QrCertificationResponseData
+import com.minionz.qrna.network.RetrofitBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class QrCodeReaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +35,35 @@ class QrCodeReaderActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+        val errorMessage = Toast.makeText(this@QrCodeReaderActivity,"인증에 실패했습니다",Toast.LENGTH_SHORT)
 
         if(result != null) {
             if(result.contents != null) {
-                Toast.makeText(this,"인증이 완료되었습니다",Toast.LENGTH_SHORT).show()
+                val requestBody = QrCertificationRequestData("","")
+
+                RetrofitBuilder.networkService.certification(requestBody).enqueue(object : Callback<QrCertificationResponseData> {
+                    override fun onFailure(call: Call<QrCertificationResponseData>, t: Throwable) {
+                        errorMessage.show()
+                    }
+
+                    override fun onResponse(
+                        call: Call<QrCertificationResponseData>,
+                        response: Response<QrCertificationResponseData>
+                    ) {
+                        if(response.isSuccessful) {
+                            Toast.makeText(
+                                this@QrCodeReaderActivity,
+                                "인증이 완료되었습니다",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        else errorMessage.show()
+
+                        finish()
+                    }
+                })
                 Log.e("data",result.contents)
-                finish()
+
             } else {
                 Toast.makeText(this,"저장된 데이터가 없습니다",Toast.LENGTH_SHORT).show()
                 finish()
