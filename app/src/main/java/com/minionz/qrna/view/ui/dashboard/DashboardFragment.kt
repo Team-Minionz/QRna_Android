4a9c69e0848662cad761fce20ffd558be46f3823
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-08 15:39:34 +0900

    feat : DashboardFragment에서 버튼 클릭시 QR 코드 인식 화면으로 이동하는 기능 추가

--- app/src/main/java/com/minionz/qrna/view/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/minionz/qrna/view/ui/dashboard/DashboardFragment.kt
@@ -1,25 +1,38 @@
 package com.minionz.qrna.view.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.zxing.integration.android.IntentIntegrator
 import com.minionz.qrna.R
+import com.minionz.qrna.databinding.FragmentDashboardBinding
+import com.minionz.qrna.view.qr.QrCodeReaderActivity
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var binding : FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+    ): View {
 
-        return root
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_dashboard,container,false)
+        binding.lifecycleOwner = this
+        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        binding.viewModel = dashboardViewModel
+
+        binding.qrButton.setOnClickListener {
+            val intent = Intent(this.requireActivity(),QrCodeReaderActivity::class.java)
+            startActivity(intent)
+        }
+        return binding.root
     }
 }
