1fb3bc4b64d981a47d2816cf35a6fca2815bc3fd
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-22 16:23:56 +0900

    feat : 점주의 매장 관리 Activity 추가

---
+++ app/src/main/java/com/minionz/qrna/view/OwnerActivity.kt
@@ -0,0 +1,12 @@
+package com.minionz.qrna.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minionz.qrna.R
+
+class OwnerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owner)
+    }
+}
