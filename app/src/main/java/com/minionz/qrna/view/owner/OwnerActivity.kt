3bdfd45b00d34025119048a63b3efa397e28da88
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-24 14:36:14 +0900

    feat : Owner 메인 Activity 구성

---
+++ app/src/main/java/com/minionz/qrna/view/owner/OwnerActivity.kt
@@ -0,0 +1,29 @@
+package com.minionz.qrna.view.owner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.minionz.qrna.R
+import com.minionz.qrna.databinding.ActivityOwnerBinding
+
+class OwnerActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityOwnerBinding
+    private lateinit var viewModel : OwnerManageViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_owner)
+        binding.lifecycleOwner = this
+        viewModel = ViewModelProvider(this).get(OwnerManageViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.inquireShop()
+    }
+}
