530bfc2e6a75bdc03bf912ad5bbaab2c686c5fef
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-29 20:55:01 +0900

    feat : 가게 등록 시 테이블 정보 추가 기능 구현

--- app/src/main/java/com/minionz/qrna/view/owner/OwnerManageViewModel.kt
+++ app/src/main/java/com/minionz/qrna/view/owner/OwnerManageViewModel.kt
@@ -1,8 +1,12 @@
 package com.minionz.qrna.view.owner
 
+import android.widget.Button
 import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.minionz.qrna.data.AddressInfo
 import com.minionz.qrna.data.StoreListData
+import com.minionz.qrna.data.TableInfoData
 import com.minionz.qrna.network.RetrofitBuilder
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,6 +15,14 @@ import retrofit2.Response
 class OwnerManageViewModel : ViewModel(){
 
     val storeList = ObservableArrayList<StoreListData>()
+    val currentTableMaxUser = MutableLiveData<String>()
+    val tableInfo = ObservableArrayList<TableInfoData>()
+
+    val storeName = MutableLiveData<String>()
+    val storeZipCode = MutableLiveData<String>()
+    val storeStreet = MutableLiveData<String>()
+    val storeCity = MutableLiveData<String>()
+    val storeTelNum = MutableLiveData<String>()
 
     fun inquireShop() {
         storeList.clear()
@@ -25,12 +37,18 @@ class OwnerManageViewModel : ViewModel(){
                 response: Response<List<StoreListData>>
             ) {
                 if(response.isSuccessful) {
-                    repeat(response.body()?.size!!) {
+                    repeat(response.body()!!.size) {
                         storeList.add(response.body()!![it])
                     }
-
                 }
             }
         })
+    }
+
+    fun addTable(button: Button,tableMaxUser : String) {
+        button.setOnClickListener {
+            tableInfo.add(TableInfoData(tableMaxUser.toInt()))
+            currentTableMaxUser.value = ""
+        }
     }
 }
