bf625b401518133e075d0aca6916b776352a3eb0
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-29 20:51:21 +0900

    feat : 가게 목록 API 요청

--- app/src/main/java/com/minionz/qrna/view/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/minionz/qrna/view/ui/home/HomeViewModel.kt
@@ -1,13 +1,39 @@
 package com.minionz.qrna.view.ui.home
 
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.ObservableArrayList
 import androidx.lifecycle.ViewModel
 import com.minionz.qrna.data.StoreListData
+import com.minionz.qrna.network.RetrofitBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeViewModel : ViewModel() {
 
-    val storeList = ObservableArrayList<StoreListData>().apply {
-        this.add(StoreListData("이름","주소","혼잡도"))
-    }
+    var storeList = ObservableArrayList<StoreListData>()
 
+    fun inquireShop() {
+        storeList.clear()
+
+        RetrofitBuilder.networkService.inquireShop().enqueue(object :
+            Callback<List<StoreListData>> {
+            override fun onFailure(call: Call<List<StoreListData>>, t: Throwable) {
+
+            }
+
+            override fun onResponse(
+                call: Call<List<StoreListData>>,
+                response: Response<List<StoreListData>>
+            ) {
+                Log.e(",,,",response.body()!!.toString())
+                if(response.isSuccessful) {
+                    repeat(response.body()!!.size) {
+                        storeList.add(response.body()!![it])
+                    }
+                }
+            }
+        })
+    }
 }
