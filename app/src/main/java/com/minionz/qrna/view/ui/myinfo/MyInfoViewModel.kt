123b97f9a96328d07be5420564ec01bd370f4c23
garam918 <62510764+garam918@users.noreply.github.com>
2021-10-29 20:51:36 +0900

    feat : 내 정보 API 요청

--- app/src/main/java/com/minionz/qrna/view/ui/myinfo/MyInfoViewModel.kt
+++ app/src/main/java/com/minionz/qrna/view/ui/myinfo/MyInfoViewModel.kt
@@ -1,10 +1,33 @@
 package com.minionz.qrna.view.ui.myinfo
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.minionz.qrna.SingleTon
+import com.minionz.qrna.data.UserInfoData
+import com.minionz.qrna.network.RetrofitBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyInfoViewModel : ViewModel() {
 
-    val userEmail = MutableLiveData<String>().apply { this.value = "" }
+    val myInfo = MutableLiveData<UserInfoData>()
+
+    fun getMyInfo() {
+        RetrofitBuilder.networkService.getMyInfo(SingleTon.prefs.userId,"USER").enqueue(object : Callback<UserInfoData> {
+            override fun onFailure(call: Call<UserInfoData>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<UserInfoData>, response: Response<UserInfoData>) {
+                Log.e("??",response.raw().message())
+                if(response.isSuccessful) {
+                    myInfo.value = response.body()!!
+                }
+            }
+        })
+
+    }
 
 }
