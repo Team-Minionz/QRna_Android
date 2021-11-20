3ee0a3ab8d2691f1cd3c774913209f4e61c28fec
garam918 <62510764+garam918@users.noreply.github.com>
2021-11-20 21:02:31 +0900

    feat : MyInfo 메뉴에 방문 기록 조회 기능 추가

--- app/src/main/java/com/minionz/qrna/util/HomeBindingAdapter.kt
+++ app/src/main/java/com/minionz/qrna/util/HomeBindingAdapter.kt
@@ -4,13 +4,15 @@ import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableArrayList
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.minionz.qrna.data.ShopInfoData
 import com.minionz.qrna.data.StoreListData
+import com.minionz.qrna.data.VisitShopInfo
 
 object HomeBindingAdapter {
 
     @BindingAdapter("itemList")
     @JvmStatic
-    fun bindItem(recyclerView: RecyclerView, items: ObservableArrayList<StoreListData>) {
+    fun bindItem(recyclerView: RecyclerView, items: ObservableArrayList<ShopInfoData>) {
         if(recyclerView.adapter == null) {
             val lm = LinearLayoutManager(recyclerView.context)
             val adapter = StoreListRecyclerAdapter()
@@ -18,6 +20,19 @@ object HomeBindingAdapter {
             recyclerView.adapter = adapter
         }
         (recyclerView.adapter as StoreListRecyclerAdapter).items = items
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    @BindingAdapter("visitList")
+    @JvmStatic
+    fun bindVisitItem(recyclerView: RecyclerView, items: ObservableArrayList<VisitShopInfo>) {
+        if(recyclerView.adapter == null) {
+            val lm = LinearLayoutManager(recyclerView.context)
+            val adapter = VisitInfoRecyclerAdapter()
+            recyclerView.layoutManager = lm
+            recyclerView.adapter = adapter
+        }
+        (recyclerView.adapter as VisitInfoRecyclerAdapter).items = items
         recyclerView.adapter?.notifyDataSetChanged()
     }
 }
